7f59bc6601d98b52227e3f462981915b64f5f267
Lauda Carly <lauda@nel.re>
2025-12-20 23:32:08 +0100

    update egui

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,16 +1,11 @@
-use eframe::egui::{self, CentralPanel, Window};
-use egui_keyboard::Keyboard;
+use eframe::egui::{self, CentralPanel};
 
 pub struct MyApp {
-    text: String,
-    keyboard: Keyboard,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            text: "edit me mycelia".to_string(),
-            keyboard: Keyboard::default(),
         }
     }
 }
@@ -18,19 +13,7 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            // Inject text events into Egui context. This function needs to be called before any widget is
-            // created, otherwise the key presses will be ignored.
-            self.keyboard.pump_events(ctx);
-
-            Window::new("Hello")
-                .constrain_to(self.keyboard.safe_rect(ctx))
-                .show(ui.ctx(), |ui| {
-                    ui.label("This window is constrained to the \"safe\" area, which is not covered by the keyboard.");
-                    ui.text_edit_singleline(&mut self.text);
-                });
-
-            // Show the keyboard.
-            self.keyboard.show(ctx);
+            ui.label("test");
         });
     }
 }
