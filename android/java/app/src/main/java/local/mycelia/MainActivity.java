7ec376e862a8d710d2457d9f73eda7346d6191e1
Lauda Carly <lauda@nel.re>
2025-12-25 11:46:56 +0100

    Clean up MainActivity.java

--- android/java/app/src/main/java/local/mycelia/MainActivity.java
+++ android/java/app/src/main/java/local/mycelia/MainActivity.java
@@ -9,46 +9,91 @@ import android.view.ViewGroup;
 import androidx.core.graphics.Insets;
 import androidx.core.view.DisplayCutoutCompat;
 import androidx.core.view.ViewCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.google.androidgamesdk.GameActivity;
 
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 public class MainActivity extends GameActivity {
-  static {
-    System.loadLibrary("main");
-  }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      // Shrink view so it does not get covered by insets.
+    static {
+        // Load the STL first to workaround issues on old Android versions:
+        // "if your app targets a version of Android earlier than Android 4.3
+        // (Android API level 18),
+        // and you use libc++_shared.so, you must load the shared library before any other
+        // library that depends on it."
+        // See https://developer.android.com/ndk/guides/cpp-support#shared_runtimes
+        //System.loadLibrary("c++_shared");
 
-      View content = getWindow().getDecorView().findViewById(android.R.id.content);
-      ViewCompat.setOnApplyWindowInsetsListener(content, (v, windowInsets) -> {
-        Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+        // Load the native library.
+        // The name "android-game" depends on your CMake configuration, must be
+        // consistent here and inside AndroidManifect.xml
+        System.loadLibrary("main");
+    }
 
-        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
-        mlp.topMargin = insets.top;
-        mlp.leftMargin = insets.left;
-        mlp.bottomMargin = insets.bottom;
-        mlp.rightMargin = insets.right;
-        v.setLayoutParams(mlp);
+    private void hideSystemUI() {
+        // This will put the game behind any cutouts and waterfalls on devices which have
+        // them, so the corresponding insets will be non-zero.
+        if (VERSION.SDK_INT >= VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode
+                    = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+        }
+        // From API 30 onwards, this is the recommended way to hide the system UI, rather than
+        // using View.setSystemUiVisibility.
+        View decorView = getWindow().getDecorView();
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(),
+                decorView);
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.hide(WindowInsetsCompat.Type.displayCutout());
+        controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
 
-        return WindowInsetsCompat.CONSUMED;
-      });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Shrink view so it does not get covered by insets.
 
-      WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        View content = getWindow().getDecorView().findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(content, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
 
-      super.onCreate(savedInstanceState);
-  }
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.topMargin = insets.top;
+            mlp.leftMargin = insets.left;
+            mlp.bottomMargin = insets.bottom;
+            mlp.rightMargin = insets.right;
+            v.setLayoutParams(mlp);
 
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-      // Offset the location so it fits the view with margins caused by insets.
+            return WindowInsetsCompat.CONSUMED;
+        });
+        // When true, the app will fit inside any system UI windows.
+        // When false, we render behind any system UI windows.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        hideSystemUI();
+        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
+        // We set the fields in native_engine.cpp.
+        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
+        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
+        super.onCreate(savedInstanceState);
+    }
 
-      int[] location = new int[2];
-      findViewById(android.R.id.content).getLocationOnScreen(location);
-      event.offsetLocation(-location[0], -location[1]);
-      return super.onTouchEvent(event);
-  }
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    public boolean isGooglePlayGames() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature("com.google.android.play.feature.HPE_EXPERIENCE");
+    }
 }
